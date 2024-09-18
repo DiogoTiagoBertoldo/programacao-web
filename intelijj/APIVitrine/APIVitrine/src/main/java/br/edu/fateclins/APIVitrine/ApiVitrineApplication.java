34134f45495b616c212b6b0fbcf19d49fb70225a
Diogo Tiago Bertoldo <139913465+AxLentrise@users.noreply.github.com>
2024-09-18 14:42:45 -0300

    update

---
+++ intelijj/APIVitrine/APIVitrine/src/main/java/br/edu/fateclins/APIVitrine/ApiVitrineApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fateclins.APIVitrine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVitrineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVitrineApplication.class, args);
+	}
+
+}
---
+++ intelijj/APIVitrine/APIVitrine/src/test/java/br/edu/fateclins/APIVitrine/ApiVitrineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fateclins.APIVitrine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVitrineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
